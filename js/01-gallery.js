acf7105a645d897c6936ff936dbf50b8f3850ddc
LazyStudent23 <wizard.alex2016@gmail.com>
2023-09-17 20:05:53 +0300

    hw7 attempt1

---
+++ js/01-gallery.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryList = document.querySelector(".gallery");
+galleryList.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+galleryList.addEventListener("click", handleClick);
+function createMarkup (arr) {
+    return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `).join("");
+    
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    // const currentItem = event.target;
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1280">
+`)
+
+instance.show()
+    console.log(event.target);
+}
+
